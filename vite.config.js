300b7a2446fd081c9a7901008c27754e3d62c514
Gketan97 <63143731+Gketan97@users.noreply.github.com>
2025-08-14 15:04:04 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', 
-  build: {
-    rollupOptions: {
-      external: ['firebase/app', 'firebase/auth', 'firebase/firestore']
-    }
-  }
 })
