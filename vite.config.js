6c6e36a9dc5c448aa935dde30c108a71fbb406e4
Gketan97 <63143731+Gketan97@users.noreply.github.com>
2025-08-18 19:18:59 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'; // <-- Add this line
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // v-- Add this 'resolve' block --v
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
